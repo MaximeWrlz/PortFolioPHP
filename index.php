91960b619b5fe47855c0372b70de8c5fb3a7d04c
Max <maximewarluzel@gmail.com>
2020-03-17 16:15:17 +0100

    Commit d'initialisation

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<?php 
+session_start();
+include_once("code.php");
+
+$user = new Users;
+?>
+
+<html lang="fr">
+<head>
+	<title>php</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<p>Bonjour <?php echo($_SESSION["account"]["username"]); ?></p>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<?php
+session_start();
+include_once("code.php");
+
+$user = new Users;
+if (isset($_SESSION["account"])){
+	header('Location: index.php');
+}
+if(isset($_POST['submit'])){
+	if ($_POST['submit'] === "OK")
+	{
+		if ($_POST['uname'] != NULL && $_POST['psw'] != NULL)
+		{
+			$user->connect($_POST["uname"], $_POST["psw"]);
+		}
+	}
+	else{echo "Remplis le formulaire";}
+}
+
+?>
+
+<html>
+<head>
+	<title>Login Page</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+	<form action="login.php" method="post">
+
+	  <div class="container">
+	    <label for="uname"><b>Username</b></label>
+	    <input type="text" placeholder="Enter Username" name="uname" required>
+
+	    <label for="psw"><b>Password</b></label>
+	    <input type="password" placeholder="Enter Password" name="psw" required>
+
+	    <button type="submit" name="submit" value="OK">Login</button>
+	  </div>
+	</form>
+
+</body>
+</html>
